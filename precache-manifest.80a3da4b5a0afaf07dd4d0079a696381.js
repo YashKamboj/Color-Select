200e03bffa136a19c59282676a56a96102a2f5a3
YashKamboj <yashkamboj29@gmail.com>
2021-08-19 18:50:26 +0530

    Updates

---
+++ precache-manifest.80a3da4b5a0afaf07dd4d0079a696381.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eceb36b46cd77a7c321c",
+    "url": "/Color-Select/static/css/main.d93b9eb4.chunk.css"
+  },
+  {
+    "revision": "eceb36b46cd77a7c321c",
+    "url": "/Color-Select/static/js/main.466b6e9e.chunk.js"
+  },
+  {
+    "revision": "cb221229c94ee019f8ce",
+    "url": "/Color-Select/static/js/runtime~main.b6cd2970.js"
+  },
+  {
+    "revision": "fc4a32b470016ca351f4",
+    "url": "/Color-Select/static/css/2.0b1dccb9.chunk.css"
+  },
+  {
+    "revision": "fc4a32b470016ca351f4",
+    "url": "/Color-Select/static/js/2.dd22e92e.chunk.js"
+  },
+  {
+    "revision": "ae3e7f292c5e415fffcc24fbdce6afad",
+    "url": "/Color-Select/static/media/spectrum-gradient.ae3e7f29.svg"
+  },
+  {
+    "revision": "9ebca2f3fead5934dcc6c0c9f335e631",
+    "url": "/Color-Select/index.html"
+  }
+];
