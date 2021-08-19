7f2d9e90c80afd9778ab9c08433fa69cd8f5f0cc
YashKamboj <yashkamboj29@gmail.com>
2021-08-19 18:17:58 +0530

    all files commit

---
+++ src/component/Palette/MiniPalette/MiniPalette.js
@@ -0,0 +1,46 @@
+import React, { PureComponent } from "react";
+
+import { withStyles } from "@material-ui/styles";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+import styles from "./MiniPaletteStyles";
+
+class MiniPalette extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.deletePalette = this.deletePalette.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  deletePalette(e) {
+    e.stopPropagation();
+    this.props.openDialog(this.props.id);
+  }
+  handleClick() {
+    this.props.goToPalette(this.props.id);
+  }
+  render() {
+    const { classes, paletteName, colors } = this.props;
+
+    const miniColorBoxes = colors.map((color) => (
+      <div
+        className={classes.miniColor}
+        style={{ backgroundColor: color.color }}
+        key={color.name}
+      />
+    ));
+    return (
+      <div className={classes.root} onClick={this.handleClick}>
+        <DeleteIcon
+          className={classes.deleteIcon}
+          style={{ transition: "all 0.3s ease-in-out" }}
+          onClick={this.deletePalette}
+        />
+
+        <div className={classes.colors}>{miniColorBoxes}</div>
+        <h5 className={classes.title}>{paletteName}</h5>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(MiniPalette);
